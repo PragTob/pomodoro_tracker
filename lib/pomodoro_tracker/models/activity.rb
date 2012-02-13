70a3974d259869c4de410b8f8a15e3d3b8c84798
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2012-02-13 15:53:44 +0100

    Status and activities update

--- lib/pomodoro_tracker/models/activity.rb
+++ lib/pomodoro_tracker/models/activity.rb
@@ -1,11 +1,26 @@
 module PomodoroTracker
   class Activity
     attr_accessor :description, :estimate, :priority
-    attr_reader   :pomodori
+    attr_reader   :pomodori, :status
 
     def initialize(description="" )
       @description = description
       @pomodori = 0
+      @status = :inactive
+    end
+
+    def start
+      @status = :active
+    end
+
+    def pause
+      @status = :paused
+      @pomodori += 1
+    end
+
+    def finish
+      @status = :finished
+      @pomodori += 1
     end
 
   end
--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe PomodoroTracker::Activity do
-  before :all do
+  before :each do
     @activity = FactoryGirl.build :activity
   end
 
@@ -23,6 +23,57 @@ describe PomodoroTracker::Activity do
 
   it "can be created given a description" do
     PomodoroTracker::Activity.new("describe me").should_not be nil
+  end
+
+  it "can be created given a description and then the description is correct" do
+    PomodoroTracker::Activity.new("describe me").description
+                                                .should == "describe me"
+  end
+
+  it "has a status of inactive" do
+    @activity.status.should be :inactive
+  end
+
+  describe "actions" do
+
+    describe "after start" do
+
+      before :each do
+        @activity.start
+      end
+
+      it "can be started and changes the status to working" do
+        @activity.status.should be :active
+      end
+
+      it "can be paused (not finished after pomodoro)" do
+        @activity.pause
+        @activity.status.should be :paused
+      end
+
+      it "has a pomodori count of 1 after one pause" do
+        @activity.pause
+        @activity.pomodori.should be 1
+      end
+
+      it "can be paused and then started again" do
+        @activity.pause
+        @activity.start
+        @activity.status.should be :active
+      end
+
+      it "can be finished" do
+        @activity.finish
+        @activity.status.should be :finished
+      end
+
+      it "has a pomodri count of 1 after one finish" do
+        @activity.finish
+        @activity.pomodori.should be 1
+      end
+
+    end
+
   end
 
 end
