a8a581940d8eb5b48d732de5365239a4b2db1bcc
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2012-12-28 14:25:08 +0100

    Namespacing and if --> case
    * pute name space in the main_windo file
    * moved all methods into the Shoes.app block
    * refactored an if statement to a case statement
    
    [ci skip] since just shoes code changed

--- lib/pomodoro_tracker/main_window.rb
+++ lib/pomodoro_tracker/main_window.rb
@@ -1,55 +1,58 @@
-# the main menu displayed on the left hand side
-# opens the specified content in the main window
-# find the corresponding classes in the tabs directory
-def menu
-  stack width: MENU_WIDTH do
-    # TODO Why doesn't para link work? o_O
-    button 'Home', width: BUTTON_WIDTH do
-      @slot_manager.open PomodoroTracker::HomeTab
+module PomodoroTrack
+  Shoes.app title: "pomodoro tracker", width: 700, height: 600 do
+    # the main menu displayed on the left hand side
+    # opens the specified content in the main window
+    # find the corresponding classes in the tabs directory
+    def menu
+      stack width: MENU_WIDTH do
+        # TODO Why doesn't para link work? o_O
+        button 'Home', width: BUTTON_WIDTH do
+          @slot_manager.open PomodoroTracker::HomeTab
+        end
+        button 'Inventory', width: BUTTON_WIDTH do open_inventory end
+        button 'ToDo Today', width: BUTTON_WIDTH do open_today end
+        button 'Close', width: BUTTON_WIDTH do close_pomodoro  end
+      end
     end
-    button 'Inventory', width: BUTTON_WIDTH do open_inventory end
-    button 'ToDo Today', width: BUTTON_WIDTH do open_today end
-    button 'Close', width: BUTTON_WIDTH do close_pomodoro  end
+
+    def open_inventory
+      @slot_manager.open PomodoroTracker::ActivityInventoryTab, @inventory
+    end
+
+    def open_today
+      @slot_manager.open PomodoroTracker::TodayTab, @inventory
+    end
+
+    def close_pomodoro
+      close if confirm "Are you sure?"
+    end
+
+    def boot
+      @inventory = PomodoroTracker::ActivityInventory.new
+    end
+
+    def general_key_handlers
+      keypress do |key|
+        case key
+          when :control_i then open_inventory
+          when :control_t then open_today
+          when :control_q then close_pomodoro
+        end
+      end
+    end
+
+    boot
+    title "Pomodoros track you must", :align => "center"
+    flow do
+      menu
+      @main_content = stack width: -MENU_WIDTH
+    end
+
+    general_key_handlers
+
+    @slot_manager = ShoesSlotManager::SlotManager.new(@main_content)
+    # we start at home!
+    @slot_manager.open(PomodoroTracker::HomeTab)
   end
-end
-
-def open_inventory
-  @slot_manager.open PomodoroTracker::ActivityInventoryTab, @inventory
-end
-
-def open_today
-  @slot_manager.open PomodoroTracker::TodayTab, @inventory
-end
-
-def close_pomodoro
-  close if confirm "Are you sure?"
-end
-
-def boot
-  @inventory = PomodoroTracker::ActivityInventory.new
-end
-
-def general_key_handlers
-  keypress do |key|
-    open_inventory if key == :control_i
-    open_today if key == :control_t
-    close_pomodoro if key == :control_q
-  end
-end
-
-# main app
-Shoes.app title: "pomodoro tracker", width: 700, height: 600 do
-  boot
-  title "Pomodoros track you must", :align => "center"
-  flow do
-    menu
-    @main_content = stack width: -MENU_WIDTH
-  end
-  
-  general_key_handlers
-
-  @slot_manager = ShoesSlotManager::SlotManager.new(@main_content)
-  # we start at home!
-  @slot_manager.open(PomodoroTracker::HomeTab)
 end
 
