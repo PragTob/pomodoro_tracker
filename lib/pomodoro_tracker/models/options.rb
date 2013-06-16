7b6a1aa608ac393d1469afe465ed4957015fe51b
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-06-16 18:20:42 +0200

    refactoring of options with Meta Programming :-)

--- lib/pomodoro_tracker/models/options.rb
+++ lib/pomodoro_tracker/models/options.rb
@@ -3,7 +3,11 @@ require 'yaml/store'
 module PomodoroTracker
   class Options
 
-    DEFAULT_POMDORO_TIME        = 25
+    OPTIONS                     = [:pomodoro_time,
+                                   :normal_pause_time,
+                                   :extended_pause_time]
+
+    DEFAULT_POMODORO_TIME        = 25
     DEFAULT_NORMAL_PAUSE_TIME   = 5
     DEFAULT_EXTENDED_PAUSE_TIME = 15
 
@@ -11,28 +15,30 @@ module PomodoroTracker
       @storage = YAML::Store.new file_path
     end
 
-    def pomodoro_time
-      get(:pomodoro_time) || DEFAULT_POMDORO_TIME
+    def self.define_getter(option)
+      default_constant = const_get(const_from(option))
+      define_method(option) do
+        get(option) || default_constant
+      end
     end
 
-    def normal_pause_time
-      get(:normal_pause_time) || DEFAULT_NORMAL_PAUSE_TIME
+    def self.const_from(option)
+      ('DEFAULT_' + option.to_s.upcase).to_sym
     end
 
-    def extended_pause_time
-      get(:extended_pause_time) || DEFAULT_EXTENDED_PAUSE_TIME
+    def self.define_setter(option)
+      define_method(setter_name_from(option)) do |value|
+        save option, value
+      end
     end
 
-    def pomodoro_time=(value)
-      save(:pomodoro_time, value)
+    def self.setter_name_from(option)
+      (option.to_s + '=').to_sym
     end
 
-    def normal_pause_time=(value)
-      save(:normal_pause_time, value)
-    end
-
-    def extended_pause_time=(value)
-      save(:extended_pause_time, value)
+    OPTIONS.each do |option|
+      define_getter(option)
+      define_setter(option)
     end
 
     private
--- lib/pomodoro_tracker/tabs/options_tab.rb
+++ lib/pomodoro_tracker/tabs/options_tab.rb
@@ -2,28 +2,35 @@ module PomodoroTracker
   class OptionsTab < ShoesSlotManager::ManageableSlot
     def init_data(options)
       @options = options
+      @option_edit_lines = {}
     end
 
     def content
       stack do
-        flow do
-          para 'Pomodoro time'
-          @pomodoro_time = edit_line @options.pomodoro_time
-        end
-        flow do
-          para 'Normal pause time'
-          @normal_pause_time = edit_line @options.normal_pause_time
-        end
-        flow do
-          para 'Extended pause time'
-          @extended_pause_time = edit_line @options.extended_pause_time
-        end
-        button 'Save' do
-          @options.pomodoro_time       = @pomodoro_time.text.to_i
-          @options.normal_pause_time   = @normal_pause_time.text.to_i
-          @options.extended_pause_time = @extended_pause_time.text.to_i
+        option_edit_lines
+        save_button()
+      end
+    end
+
+    private
+    def option_edit_lines
+      Options::OPTIONS.each do |option|
+        para description_from(option)
+        @option_edit_lines[option] = edit_line @options.send(option)
+      end
+    end
+
+    def save_button
+      button 'Save' do
+        Options::OPTIONS.each do |option|
+          setter_name = Options.setter_name_from option
+          @options.send(setter_name, @option_edit_lines[option].text.to_i)
         end
       end
+    end
+
+    def description_from(option)
+      option.to_s.gsub('_', ' ').capitalize
     end
   end
 end
