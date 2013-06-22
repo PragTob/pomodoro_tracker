91cfbac337863466a5d6fd0bed845afab4589108
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-06-22 19:22:47 +0200

    a bit of refactoring

--- lib/after_do/after_do.rb
+++ lib/after_do/after_do.rb
@@ -15,12 +15,7 @@ module AfterDo
       raise ArgumentError, 'after takes at least one method name!'
     end
     methods.each do |method|
-      aliased_name = alias_name method
-      if !private_method_defined? aliased_name
-        @_after_do_callbacks[method] = []
-        rename_old_method(method, aliased_name)
-        redefine_method_with_callback(aliased_name, method)
-      end
+      make_after_do_version_of_method(method) if method_already_renamed?(method)
       @_after_do_callbacks[method] << block
     end
   end
@@ -32,7 +27,14 @@ module AfterDo
   end
 
   private
-  def alias_name(symbol)
+  def make_after_do_version_of_method(method)
+    @_after_do_callbacks[method] = []
+    alias_name = aliased_name method
+    rename_old_method(method, alias_name)
+    redefine_method_with_callback(method, alias_name)
+  end
+
+  def aliased_name(symbol)
     (ALIAS_PREFIX + symbol.to_s).to_sym
   end
 
@@ -43,15 +45,19 @@ module AfterDo
     end
   end
 
-  def redefine_method_with_callback(aliased_name, method)
+  def redefine_method_with_callback(method, alias_name)
     class_eval do
       define_method method do |*args|
-        return_value = send(aliased_name, *args)
+        return_value = send(alias_name, *args)
         self.class._after_do_callbacks[method].each do |block|
           block.call *args, self
         end
         return_value
       end
     end
+  end
+
+  def method_already_renamed?(method)
+    !private_method_defined? aliased_name(method)
   end
 end
